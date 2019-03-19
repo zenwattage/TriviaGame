57609965ecb3961f6253362d2cb1820512d94864
Scott H <zenwattage@gmail.com>
2019-03-19 10:29:18 -0700

    beginning pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+$(document).on('ready', function() {
+
+    console.log("This bitch is ready!");
+    $("#status").text("This bitch is ready!");
+
+    //ON START BUTTON CLICK
+
+        //DISPLAY QUESTION
+        //DISPLAY TIMER
+        //QUESTION TIMER STARTS COUNTING DOWN
+
+
+
+
+    //ARRAY OF QUESTIONS
+    //MATCHING ANSWERS
+    //CORRECT OR INCORRECT ANSWER CLICKED CHECK
+    //QUESTION TIMER
+    //TIMER STATUS CHECK
+    //
+
+    
+    //ON ANSWER CLICK
+        // RETURN CORRECT OR INCORRECT
+    
+        //IF CORRECT
+            //DISPLAY MESSAGE "THAT'S CORRECT!"
+            //DISPLAY IMAGE
+
+        //IF INCORRECT
+            //RETURN CORRECT ANSWER
+            //DISPLAY IMAGE
+            // FOR SET DURATION
+        
+        //MOVE TO NEXT QUESTION
+
+
+    //IF TIMER REACHES ZERO
+        // DISPLAY OUT OF TIME
+        //DISPLAY CORRECT ANSWER
+        // MOVE ON TO NEXT QUESTION
+
+
+    //WHEN GAME ENDS
+        //TIMER STOPS
+        //DISPLAY CORRECT,INCORRECT, AND SKIPPED COUNTS
+        //DISPLAY RESET BUTTON
+
+        //ON RESET BUTTON CLICK
+        //RESET GAME
+
+
+
+
+
+
+
+
+} );
